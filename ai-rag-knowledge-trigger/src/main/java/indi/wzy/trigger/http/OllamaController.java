0e53672b766f1b14ae393e1a047dcdf66fec307b
kkkkendei <authoryu0607@gmail.com>
2025-06-09 20:06:01 +0800

    feat: 前端页面

--- ai-rag-knowledge-trigger/src/main/java/indi/wzy/trigger/http/OllamaController.java
+++ ai-rag-knowledge-trigger/src/main/java/indi/wzy/trigger/http/OllamaController.java
@@ -30,7 +30,7 @@ public class OllamaController implements IAIService {
     }
 
     /**
-     * http://localhost:8090/api/v1/ollama/generate?model=deepseek-r1:1.5b&message=hi
+     * http://localhost:8090/api/v1/ollama/generate?model=deepseek-r1:1.5b&message=1 plus 1
      */
     @RequestMapping(value = "/generate", method = RequestMethod.GET)
     @Override
@@ -39,7 +39,7 @@ public class OllamaController implements IAIService {
     }
 
     /**
-     * http://localhost:8090/api/v1/ollama/generateStream?model=deepseek-r1:1.5b&message=1+1
+     * http://localhost:8090/api/v1/ollama/generateStream?model=deepseek-r1:1.5b&message=1 plus 1
      */
     @RequestMapping(value = "/generateStream", method = RequestMethod.GET)
     @Override
