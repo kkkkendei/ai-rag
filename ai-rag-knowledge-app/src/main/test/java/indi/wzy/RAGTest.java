7877385a280dd4504c1b19415ad1fa7ed4e06c08
kkkkendei <authoryu0607@gmail.com>
2025-06-11 17:12:34 +0800

    feat: 向量化存储

---
+++ ai-rag-knowledge-app/src/main/test/java/indi/wzy/RAGTest.java
@@ -0,0 +1,21 @@
+package indi.wzy;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author wuzeyu
+ * @description
+ * @github github.com/kkkkendei
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RAGTest {
+
+
+
+}
